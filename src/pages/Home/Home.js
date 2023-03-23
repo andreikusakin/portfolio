a18da2c11239bdf1d97d3f103a2fc7068e011931
Andrew Kusakin <andrewkusakin@Andrews-MacBook-Pro-2.local>
2023-03-23 01:17:59 -0400

    initial commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
+import { motion } from "framer-motion";
+
+const menuItem = [
+    {
+        name: "Work",
+        link: ""
+    },
+    {
+        name: "About",
+        link: ""
+    },
+    {
+        name: "Contact",
+        link: ""
+    },
+]
 
 export default function Home() {
   return (
-    <div>
-        <div></div>
-
-
+    <div className="home-page">
+      <div className="home-page_content">
+        <div className="home-page_text">
+          <h3>Andrei kusakin</h3>
+          <p>Web designer & developer</p>
+        </div>
+        <div className="middle-line">
+          <span></span>
+        </div>
+        <nav>
+          <ul className="home-nav">
+            {menuItem.map((item) => (
+              <li>
+                <a href={item.link}>
+                  <motion.div
+                    whileHover={{ x: 20 }}
+                    transition={{ type: "tween", duration: 0.3 }}
+                  >
+                    {item.name}
+                  </motion.div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <div className="home-socials">
+        <ul>
+          <li>
+            <a>linkedin</a>
+          </li>
+          <li>
+            <a>github</a>
+          </li>
+          <li>
+            <a>twitter</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
