fb24212a035f5f83450b9ccaa369ab0ecf89d213
Andrew Kusakin <andrewkusakin@Andrews-MacBook-Pro-2.local>
2023-03-23 16:30:26 -0400

    add animation to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from './pages/Home/Home';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
     <div className="App">
-      <div className='background'></div>
+      <motion.div
+      initial={{ opacity: 0}}
+      animate={{ opacity: 2 }}
+      transition={{ duration: 2, delay: 2 }}
+      className='background'></motion.div>
       <Home />
     </div>
         
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,60 +2,96 @@ import React from "react";
 import "./home.css";
 import { motion } from "framer-motion";
 
+const socials = [
+  {
+    name: "linkedin",
+    link: "",
+  },
+  {
+    name: "github",
+    link: "",
+  },
+  {
+    name: "twitter",
+    link: "",
+  },
+];
+
 const menuItem = [
-    {
-        name: "Work",
-        link: ""
-    },
-    {
-        name: "About",
-        link: ""
-    },
-    {
-        name: "Contact",
-        link: ""
-    },
-]
+  {
+    name: "Work",
+    link: "",
+  },
+  {
+    name: "About",
+    link: "",
+  },
+  {
+    name: "Contact",
+    link: "",
+  },
+];
 
 export default function Home() {
   return (
     <div className="home-page">
       <div className="home-page_content">
         <div className="home-page_text">
-          <h3>Andrei kusakin</h3>
-          <p>Web designer & developer</p>
+          <motion.div
+            initial={{ x: 480 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 2, delay: 1 }}
+            style={{ marginRight: "2rem" }}
+          >
+            <h3>Andrei kusakin</h3>
+            <p>Web designer & developer</p>
+          </motion.div>
         </div>
-        <div className="middle-line">
-          <span></span>
-        </div>
-        <nav>
-          <ul className="home-nav">
-            {menuItem.map((item) => (
-              <li>
+        <motion.span
+          initial={{ height: 0 }}
+          animate={{ height: "14em" }}
+          transition={{ duration: 1 }}
+          className="middle-line"
+        ></motion.span>
+        <nav className="home-nav">
+          <motion.ul className="list-none" transition={{ staggerChildren: 2 }}>
+            {menuItem.map((item, i) => (
+              <motion.li
+                initial={{ x: -420 }}
+                animate={{ x: 0 }}
+                transition={{ duration: 2, delay: 1 + i * 0.5 }}
+              >
                 <a href={item.link}>
                   <motion.div
-                    whileHover={{ x: 20 }}
+                    className="home-menu_item"
+                    whileHover={{ x: 20, fontVariationSettings: "slnt: -10" }}
                     transition={{ type: "tween", duration: 0.3 }}
                   >
                     {item.name}
                   </motion.div>
                 </a>
-              </li>
+              </motion.li>
             ))}
-          </ul>
+          </motion.ul>
         </nav>
       </div>
+
       <div className="home-socials">
         <ul>
-          <li>
-            <a>linkedin</a>
-          </li>
-          <li>
-            <a>github</a>
-          </li>
-          <li>
-            <a>twitter</a>
-          </li>
+          {socials.map((item, i) => (
+            <motion.li className="home-socials_item"
+            whileHover={{ x: 5 }}
+                >
+              <a
+                
+              href={item.link}>
+                <motion.div
+                initial={{ y: 50}}
+                animate={{ y: 0 }}
+                transition={{ duration: 1, delay: 2 + (i * 0.3) }} >{item.name}</motion.div>
+              </a>
+            </motion.li>
+          ))}
         </ul>
       </div>
     </div>
