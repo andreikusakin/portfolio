977bea3e2c1c8844156771e3dfbf7668ae76d7a3
andrewkusakin <akusaki@wgu.edu>
2023-07-06 22:14:37 -0400

    added about page content

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -41,13 +41,25 @@ const menuItem = [
 export default function About() {
   return (
     <div className="about-page">
+      <div className="blur-background"></div>
+      
+      <div className="about-content">
+      <div className="about-photo"><img alt="" src="https://i.imgur.com/AnIudvv.jpg"/></div>
+        <div className="about-title"><h2>About</h2>
+        <hr></hr></div>
+        
+        <div className="about-text"><p>Hi there! I'm Andrei Kusakin, a full-stack web developer with a degree in Software Engineering. I love crafting websites and applications that are not only easy to use but also look great.</p>
+        <p>Beyond coding, I enjoy making music and photography in my free time. It helps me to balance my technical work and keeps me creative.</p></div>
+        
+        <a href="">resume</a>
+      </div>
       <nav className="about-nav">
         <motion.ul
           className="nav-list"
           variants={containerMenu}
           initial="hidden"
           animate="visible"
-          exit={{ x: 200, opacity: 0 }}
+          exit={{ opacity: 0 }}
         >
           {menuItem.map((item, i) => (
             <Link to={item.link} key={i}>
