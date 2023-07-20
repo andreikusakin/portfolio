3a3aa907f3c136196e6a97ff475a0735abaa8827
andrewkusakin <akusaki@wgu.edu>
2023-07-20 13:01:37 -0400

    added animations

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -15,7 +15,7 @@ const containerMenu = {
 };
 
 const listItemMenu = {
-  hidden: {  x: 200, opacity: 0 },
+  hidden: { x: 200, opacity: 0 },
   visible: { x: 0, opacity: 1 },
 };
 
@@ -41,17 +41,65 @@ const menuItem = [
 export default function About() {
   return (
     <div className="about-page">
-      <div className="blur-background"></div>
-      
+      <motion.div
+        className="blur-background"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        exit={{ opacity: 0, transition:{ duration: 1.5, delay: .5 } }}
+      ></motion.div>
+
       <div className="about-content">
-      <div className="about-photo"><img alt="" src="https://i.imgur.com/AnIudvv.jpg"/></div>
-        <div className="about-title"><h2>About</h2>
-        <hr></hr></div>
-        
-        <div className="about-text"><p>Hi there! I'm Andrei Kusakin, a full-stack web developer with a degree in Software Engineering. I love crafting websites and applications that are not only easy to use but also look great.</p>
-        <p>Beyond coding, I enjoy making music and photography in my free time. It helps me to balance my technical work and keeps me creative.</p></div>
-        
-        <a href="">resume</a>
+        <motion.div
+          className="about-photo"
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.5, delay: 1 }}
+          exit={{ opacity: 0, transition: { duration: 1, delay: 0 } }}
+        >
+          <img alt="" src="https://i.imgur.com/AnIudvv.jpg" />
+        </motion.div>
+        <div className="about-title">
+          <motion.h2
+            initial={{ x: -150, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+            exit={{ x: 100, opacity: 0 }}
+          >
+            About
+          </motion.h2>
+          <motion.hr
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            exit={{ opacity: 0, transition: { duration: 0.5 } }}
+          ></motion.hr>
+        </div>
+
+        <motion.div className="about-text"
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 1 }}
+          exit={{ opacity: 0, x: -50, transition: { duration: 1 } }}
+        >
+          <p>
+            Hi there! I'm Andrei Kusakin, a full-stack web developer with a
+            degree in Software Engineering. I love crafting websites and
+            applications that are not only easy to use but also look great.
+          </p>
+          <p>
+            Beyond coding, I enjoy making music and photography in my free time.
+            It helps me to balance my technical work and keeps me creative.
+          </p>
+        </motion.div>
+
+        <motion.a href=""
+          className="resume-link"
+          initial={{y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 1.5 }}
+          exit={{ y: 30, opacity: 0, transition: { duration: 1 } }}
+        >resume</motion.a>
       </div>
       <nav className="about-nav">
         <motion.ul
@@ -66,13 +114,13 @@ export default function About() {
               <motion.li
                 variants={listItemMenu}
                 className="about-menu-item"
-                
                 transition={{ duration: 1 }}
               >
                 <motion.div
-                whileHover={{ fontVariationSettings: "'wdth' 600" }}
-                transition={{ duration: 0.3 }}>
-                {item.name}
+                  whileHover={{ fontVariationSettings: "'wdth' 600" }}
+                  transition={{ duration: 0.3 }}
+                >
+                  {item.name}
                 </motion.div>
               </motion.li>
             </Link>
