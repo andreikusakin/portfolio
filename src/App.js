b1b31340fbaf549d27d934030d9e834f9b7489af
andrewkusakin <akusaki@wgu.edu>
2023-07-24 21:54:26 -0400

    updated routes and added menu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Menu from "./components/menu/Menu";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 function App() {
   const location = useLocation();
+  console.log(location)
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  useEffect(() => {
+    const path = location.pathname;
+    if (path === '/') {
+      setMenuVisible(false);
+    } else {
+      setMenuVisible(true);
+    }
+  }, [location]);
 
   return (
     <div className="App">
@@ -15,10 +30,12 @@ function App() {
         transition={{ duration: 2, delay: 2 }}
         className="background"
       ></motion.div>
+      <Menu visible={menuVisible} />
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
-          <Route index element={<Home />} />
-          <Route path="About" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </AnimatePresence>
     </div>
