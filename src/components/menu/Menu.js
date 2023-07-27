19c554ffb5bdd9d1fa52985e2bdd134c354dfc03
andrewkusakin <akusaki@wgu.edu>
2023-07-26 23:53:28 -0400

    added animations

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -10,18 +10,23 @@ const containerMenu = {
     opacity: 1,
     transition: {
       staggerChildren: 0.5,
-      delayChildren: 1,
+      delayChildren: 0.5,
     },
   },
-  exit: { opacity: 0, display: "none" },
 };
 
 const containerMenuFullScreen = {
-  hidden: { opacity: 0 },
+  hidden: { opacity: 0, display: "none" },
   visible: {
     opacity: 1,
+    display: "flex",
   },
-  exit: { opacity: 0, display: "none" },
+  exit: {
+    opacity: 0,
+    transitionEnd: {
+      display: "none",
+    },
+  },
 };
 
 const listItemMenu = {
@@ -51,15 +56,20 @@ const menuItem = [
 const background = {
   hidden: { opacity: 0 },
   visible: {
+    display: "block",
     opacity: 1,
     transition: {
       duration: 1.5,
     },
   },
-  exit: { opacity: 0, transition: { duration: 1.5, delay: 0.5 } },
+  exit: {
+    opacity: 0,
+    transition: { duration: 1.5, delay: 0.5 },
+    // transitionEnd: {
+    //   display: "none",
+    // },
+  },
 };
-
-
 
 const top = {
   rest: { rotate: 0, y: 0 },
@@ -67,10 +77,10 @@ const top = {
     y: 12,
     rotate: 45,
     transition: {
-      duration: 0.2
-    }
-  }
-}
+      duration: 0.2,
+    },
+  },
+};
 
 const middle = {
   rest: { fillOpacity: 0, x: 0, opacity: 1 },
@@ -82,10 +92,10 @@ const middle = {
     fillOpacity: 1,
     x: 5,
     transition: {
-      duration: 0.2
-    }
-  }
-}
+      duration: 0.2,
+    },
+  },
+};
 
 const bottom = {
   rest: { rotate: 0, y: 0 },
@@ -93,23 +103,39 @@ const bottom = {
     y: -12,
     rotate: -45,
     transition: {
-      duration: 0.2
-    }
-  }
-}
+      duration: 0.2,
+    },
+  },
+};
 
 export default function Menu({ visible }) {
+  const location = useLocation();
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
-  const location = useLocation();
-
   return (
-    <div>
-      <div className="menu-btn">
+    <motion.div
+    // initial={{ display: "none" }}
+    // animate={visible ? { display: "block" } : { display: "none" }}
+    >
+      <motion.div
+        className="blur-background"
+        variants={background}
+        initial="hidden"
+        animate={visible ? "visible" : "exit"}
+        exit="exit"
+      ></motion.div>
+      
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={visible ? { opacity: 1 } : { opacity: 0 }}
+        transition={{ duration: 0.4 }}
+        exit={{ opacity: 0 }}
+        className="menu-btn"
+      >
         <motion.svg
           initial="rest"
           whileHover="hover"
-          onClick={() => setMenuIsOpen(menuIsOpen => !menuIsOpen)}
+          onClick={() => setMenuIsOpen((menuIsOpen) => !menuIsOpen)}
           animate={menuIsOpen ? "open" : "rest"}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 52 52"
@@ -118,17 +144,36 @@ export default function Menu({ visible }) {
           className="svg-btn"
         >
           <title>Menu</title>
-          <motion.rect variants={top}  x="4" y="8" width="40px" height="6px" rx="3" ry="3"></motion.rect>
-          <motion.rect variants={middle} x="4" y="20" width="40px" height="6px" rx="3" ry="3"></motion.rect>
-          <motion.rect variants= {bottom} x="4" y="32" width="40px" height="6px" rx="3" ry="3"></motion.rect>
+          <motion.rect
+            variants={top}
+            x="4"
+            y="8"
+            width="40px"
+            height="6px"
+            rx="3"
+            ry="3"
+          ></motion.rect>
+          <motion.rect
+            variants={middle}
+            x="4"
+            y="20"
+            width="40px"
+            height="6px"
+            rx="3"
+            ry="3"
+          ></motion.rect>
+          <motion.rect
+            variants={bottom}
+            x="4"
+            y="32"
+            width="40px"
+            height="6px"
+            rx="3"
+            ry="3"
+          ></motion.rect>
         </motion.svg>
-      </div>
-      <motion.div
-        className="blur-background"
-        variants={background}
-        initial="hidden"
-        animate={visible ? "visible" : "exit"}
-      ></motion.div>
+      </motion.div>
+
       <motion.nav className="side-menu">
         <motion.ul
           className="nav-list"
@@ -164,41 +209,44 @@ export default function Menu({ visible }) {
           ))}
         </motion.ul>
       </motion.nav>
-
-      {menuIsOpen ? (
-        <motion.nav
-          variants={containerMenuFullScreen}
-          initial="hidden"
-          animate={visible ? "visible" : "exit"}
-          className="menu-fullscreen"
-        >
+      <motion.nav
+        variants={containerMenuFullScreen}
+        initial="hidden"
+        animate={menuIsOpen ? "visible" : "hidden"}
+        exit="exit"
+        className="menu-fullscreen"
+      >
+        <div className="menu-fullscreen-bg">
           <ul>
-          {menuItem.map((item, i) => (
-            <Link to={item.link} key={i}>
-              <motion.li
-                variants={listItemMenu}
-                className="side-menu-item-fullscreen"
-                style={{
-                  color:
-                    location.pathname === item.link ? "transparent" : "white",
-                  WebkitTextStroke:
-                    location.pathname === item.link ? "1px white" : "0px",
-                }}
-                transition={{ duration: 1 }}
+            {menuItem.map((item, i) => (
+              <Link
+                to={item.link}
+                key={i}
+                onClick={() => setMenuIsOpen((menuIsOpen) => !menuIsOpen)}
               >
-                <motion.div
-                  whileHover={{ fontVariationSettings: "'wdth' 600" }}
-                  transition={{ duration: 0.3 }}
+                <motion.li
+                  variants={listItemMenu}
+                  className="side-menu-item-fullscreen"
+                  style={{
+                    color:
+                      location.pathname === item.link ? "transparent" : "white",
+                    WebkitTextStroke:
+                      location.pathname === item.link ? "1px white" : "0px",
+                  }}
+                  transition={{ duration: 1 }}
                 >
-                  {item.name}
-                </motion.div>
-              </motion.li>
-            </Link>
-          ))}</ul>
-        </motion.nav>
-      ) : (
-        <Outlet />
-      )}
-    </div>
+                  <motion.div
+                    whileHover={{ fontVariationSettings: "'wdth' 600" }}
+                    transition={{ duration: 0.3 }}
+                  >
+                    {item.name}
+                  </motion.div>
+                </motion.li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </motion.nav>
+    </motion.div>
   );
 }
