55ce1f07d0a806f774bab5f36c74320b914e5e3a
andreikusakin <andrewkusakin@me.com>
2024-08-09 16:45:13 -0400

    add polyscape

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -1,32 +1,42 @@
 export const projectsList = [
-    {
-      name: "Aspen Grove",
-      image: "/aspengrove.jpg",
-      year: 2022,
-      category: "design & development",
-      url: "https://www.aspengroveofficial.com/",
-      techStack: "React, Framer Motion",
-      description:
-        "Daniel Isaenko, known as Aspen Grove, is a singer-songwriter and producer from Ukraine. He has released two albums, one EP, and several singles. His music is a blend of indie, folk, and electronic.",
-    },
-    {
-      name: "Andrew Kusakin Photography",
-      image: "/akphotography.jpg",
-      year: 2022,
-      category: "design & development",
-      url: "https://kusakinphoto.com/",
-      techStack: "React, Framer Motion",
-      description:
-        "A digital showcase of my photography work - from weddings and portraits to travel shots. Designed with simplicity, it's a mix of my web development and photo skills.",
-    },
-    {
-      name: "VisionVault",
-      image: "/visionvault.jpg",
-      year: 2022,
-      category: "design & development",
-      techStack: "React, Framer Motion, Spring Boot",
-      url: "https://wsuvpwepbz.us-east-1.awsapprunner.com/",
-      description:
-        "VisionVault is a platform for photographers to deliver, and share their photos to clients. It provides a user-friendly and aesthetically pleasing interface for both the photographers and their clients.",
-    },
-  ];
+  {
+    name: "PolyScape",
+    image: "/polyscape.jpg",
+    year: 2024,
+    category: "design & development",
+    url: "https://www.polyscape.com/",
+    techStack: "Next.js, Framer Motion, Three.js, Tone.js",
+    description:
+      "PolyScape is a web synthesizer with 3D visualization, offering an immersive experience for creating and manipulating sounds. This project leverages modern web technologies to provide a rich, interactive interface for sound synthesis and visualization.",
+  },
+  {
+    name: "Aspen Grove",
+    image: "/aspengrove.jpg",
+    year: 2023,
+    category: "design & development",
+    url: "https://www.aspengroveofficial.com/",
+    techStack: "React, Framer Motion",
+    description:
+      "Daniel Isaenko, known as Aspen Grove, is a singer-songwriter and producer from Ukraine. He has released two albums, one EP, and several singles. His music is a blend of indie, folk, and electronic.",
+  },
+  {
+    name: "Andrew Kusakin Photography",
+    image: "/akphotography.jpg",
+    year: 2023,
+    category: "design & development",
+    url: "https://kusakinphoto.com/",
+    techStack: "React, Framer Motion",
+    description:
+      "A digital showcase of my photography work - from weddings and portraits to travel shots. Designed with simplicity, it's a mix of my web development and photo skills.",
+  },
+  {
+    name: "VisionVault",
+    image: "/visionvault.jpg",
+    year: 2023,
+    category: "design & development",
+    techStack: "React, Framer Motion, Spring Boot",
+    url: "https://wsuvpwepbz.us-east-1.awsapprunner.com/",
+    description:
+      "VisionVault is a platform for photographers to deliver, and share their photos to clients. It provides a user-friendly and aesthetically pleasing interface for both the photographers and their clients.",
+  },
+];
