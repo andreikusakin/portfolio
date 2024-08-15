fb4f59024531d52ae4ddf49370a853ad1ce8df46
andreikusakin <andrewkusakin@me.com>
2024-08-14 23:39:05 -0400

    remade work page

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 import { FiArrowUpRight } from "react-icons/fi";
 import { projectsList } from "../../data/projectsList";
 
-
 const workLeftAnimations = {
   hidden: { opacity: 0 },
   visible: {
@@ -25,7 +24,7 @@ const leftListItem = {
 };
 
 export default function Work() {
-  const [mouseHover, setMouseHover] = useState("");
+  const [mouseHover, setMouseHover] = useState(null);
 
   return (
     <div className="work-page">
@@ -59,10 +58,7 @@ export default function Work() {
                 transition={{ duration: 1 }}
                 key={index}
                 onMouseEnter={() => {
-                  setMouseHover(`${p.name}`);
-                }}
-                onMouseLeave={() => {
-                  setMouseHover("");
+                  setMouseHover(p);
                 }}
               >
                 <motion.h3
@@ -78,49 +74,36 @@ export default function Work() {
             ))}
           </motion.ul>
         </div>
-        <div className="work-right">
-          {projectsList.map((p, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: p.name === mouseHover ? 1 : 0 }}
-              transition={{ duration: 0.5 }}
-              className="project-section"
-            >
-              <motion.div
-                className="project-photo"
-                initial={{ y: 50, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 1.5, delay: 1 }}
-                exit={{ opacity: 0, transition: { duration: 1, delay: 0 } }}
-                style={{ backgroundImage: `url(${p.image})` }}
-              ></motion.div>
 
-              <motion.div
-                className="project-description"
-                initial={{ opacity: 0, x: 100 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1, delay: 1 }}
-                exit={{ opacity: 0, x: -50, transition: { duration: 1 } }}
-              >
-                <p>{p.description}</p>
-              </motion.div>
-
-              <motion.div
-                className="project-category"
-                initial={{ opacity: 0, x: 100 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1, delay: 1 }}
-                exit={{ opacity: 0, x: -50, transition: { duration: 1 } }}
-              >
-                <p>{p.category}</p>
-                {p.techStack.split(", ").map((tech, i) => (
-                  <div key={i}>{tech}</div>
-                ))}
-              </motion.div>
-            </motion.div>
-          ))}
-        </div>
+        <motion.div
+          className="project"
+          style={{
+            opacity: mouseHover ? 1 : 0,
+            transition: "opacity 0.5s ease",
+          }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: mouseHover ? 1 : 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          {mouseHover && (
+            <>
+              <img src={mouseHover.image} alt={mouseHover.name} />
+              <div className="project-info">
+                <div className="project-description">
+                  {mouseHover.description}
+                </div>
+                <div>
+                  <div className="project-category">{mouseHover.category}</div>
+                  <div>
+                    {mouseHover.techStack.split(",").map((tech, index) => (
+                      <div key={index}>{tech}</div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
+        </motion.div>
       </div>
     </div>
   );
